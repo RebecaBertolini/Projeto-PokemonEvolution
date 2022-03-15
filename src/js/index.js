2261fb38deb79d1f23bd3bf1fb21d31a6f990694
RebecaBertolini <97201980+RebecaBertolini@users.noreply.github.com>
2022-03-15 17:32:16 -0300

    primeiro commit

---
+++ src/js/index.js
@@ -0,0 +1,95 @@
+// Esconder o cartão atual e mostrar o selecionado.
+
+//criar duas variáveis para a troca de tela
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonCartao = document.querySelectorAll('.cartao-pokemon')
+
+const avancar = document.getElementById('proximo')
+const voltar = document.getElementById('anterior')
+
+let numeroEvolucao = 0
+
+//adicionar evento de clique feito pelo usuário
+listaSelecaoPokemons.forEach(pokemon => {
+    //quando o click acontecer, executa a arrowFunction da frente
+    pokemon.addEventListener('click', () => {
+
+        
+        //clicar no pokemon da lista, pegar o id e guardar numa variável
+        const idPokemonSelecionado = pokemon.attributes.id.value
+        //remove a classe atual (se ela existir) para que sempre inicie no primeiro pokemon e inicia o cartão apenas com a seta de avançar
+        if (document.querySelector('.atual')) {
+            document.querySelector('.atual').classList.remove('atual')
+            avancar.classList.remove('some')
+            voltar.classList.add('some')
+            numeroEvolucao = 0
+        }
+        
+
+        //remover a classe aberto dos cartões
+        const cartaoPokemonAberto = document.querySelectorAll('.aberto')
+        cartaoPokemonAberto.forEach(function fecharCadaCartao () {
+            for (let i = 0; i < cartaoPokemonAberto.length; i++) {
+                cartaoPokemonAberto[i].classList.remove('aberto')
+            }
+        })
+
+        //colocar a classe aberto no cartão correspondente
+        // const idCartaoSelecionado = 'cartao-' + idPokemonSelecionado
+        const cartaoPokemonParaAbrir = document.querySelectorAll('.' + idPokemonSelecionado)
+        cartaoPokemonParaAbrir.forEach(function abrirCartao(){
+            for(let i = 0; i < cartaoPokemonParaAbrir.length; i++) {
+                cartaoPokemonParaAbrir[i].classList.add('aberto')
+            }
+        })
+        
+
+        //remover a classe ativo no item da lista selecionado
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        //adicionar a classe ativo no item da lista selecionado
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+    })
+}) 
+
+
+avancar.addEventListener('click', () => {
+     
+    numeroEvolucao++
+    console.log(numeroEvolucao)
+    //saber qual pokemon está ativo
+    let PokemonAtivo = document.querySelector('.ativo')
+    let idPokemonAtivo = PokemonAtivo.attributes.id.value
+    let pokemonEvolucao = document.getElementById('cartao-' + idPokemonAtivo + numeroEvolucao)
+    
+    pokemonEvolucao.classList.add('atual')  
+
+    if (numeroEvolucao == 1) {
+        voltar.classList.remove('some')
+    } else if (numeroEvolucao == 2){
+        avancar.classList.add('some')
+    }
+})
+
+voltar.addEventListener('click', () => {
+    
+    //remove a classe atual
+    let PokemonAtivo = document.querySelector('.ativo')
+    let idPokemonAtivo = PokemonAtivo.attributes.id.value
+    let pokemonEvolucao = document.getElementById('cartao-' + idPokemonAtivo + numeroEvolucao)
+    
+    pokemonEvolucao.classList.remove('atual')
+    numeroEvolucao = numeroEvolucao - 1
+    console.log(numeroEvolucao)
+    
+    if (numeroEvolucao == 1) {
+        voltar.classList.remove('some')
+        avancar.classList.remove('some')
+    } else if (numeroEvolucao == 2) {
+        avancar.classList.add('some')
+        } else if (numeroEvolucao == 0) {
+            voltar.classList.add('some')
+        }
+})
---
+++ src/readme.md
@@ -0,0 +1,33 @@
+# PokedexEvolution
+
+## Sobre o projeto:
+
+Este projeto foi desenvolvido durante a semana DEV EM DOBRO com o objetivo de praticar criação de aplicações usando HTML, CSS e JavaScript.
+
+O objetivo foi criar uma interface com o tema de 'cards de Pokemon', na qual seria possível realizar a mudança dos cartões de acordo com a escolha do pokemon feita pelo usuário. Para incrementar o projeto e como forma de desafio pessoal, também foi determinado que deveria ser possível visualizar as evoluções de cada pokemon escolhido através de setas presentes nos cartões.
+
+## Layout do projeto:
+
+![](src/imagens/layout.jpg)
+
+## Tecnologias utilizadas:
+
+* HTML5
+* CSS3
+* JavaScript
+
+## Autor
+
+*Rebeca Bertolini Spósito*
+
+Linkedin:
+<https://www.linkedin.com/in/rebeca-bertolini-spósito-544048200/>
+
+E-mail:
+<rebeca.bertolinii@gmail.com>
+
+
+
+
+
+
